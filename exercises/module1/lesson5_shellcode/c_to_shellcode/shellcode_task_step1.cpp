183222a22c6f388fcce5ad419ed12e52244244b2
hasherezade <hasherezade@gmail.com>
2021-03-23 22:17:26 +0100

    [module1][exercises] Added exercise for shellcode

---
+++ exercises/module1/lesson5_shellcode/c_to_shellcode/shellcode_task_step1.cpp
@@ -0,0 +1,186 @@
+/*
+Compile to assembly:
+ cl /c /FA <file_name>.cpp 
+ */
+#include <Windows.h>
+
+#ifdef DEVEL
+#include <iostream>
+#endif // DEVEL
+#pragma once
+
+#include <Windows.h>
+
+#ifndef TO_LOWERCASE
+#define TO_LOWERCASE(out, c1) (out = (c1 <= 'Z' && c1 >= 'A') ? c1 = (c1 - 'A') + 'a': c1)
+#endif
+
+#ifndef __NTDLL_H__
+
+typedef struct _UNICODE_STRING
+{
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR  Buffer;
+
+} UNICODE_STRING, * PUNICODE_STRING;
+
+typedef struct _PEB_LDR_DATA
+{
+    ULONG Length;
+    BOOLEAN Initialized;
+    HANDLE SsHandle;
+    LIST_ENTRY InLoadOrderModuleList;               // Points to the loaded modules (main EXE usually)
+    LIST_ENTRY InMemoryOrderModuleList;             // Points to all modules (EXE and all DLLs)
+    LIST_ENTRY InInitializationOrderModuleList;
+    PVOID      EntryInProgress;
+
+} PEB_LDR_DATA, * PPEB_LDR_DATA;
+
+//here we don't want to use any functions imported form extenal modules
+
+typedef struct _LDR_MODULE {
+    LIST_ENTRY  InLoadOrderModuleList;//   +0x00 
+    LIST_ENTRY  InMemoryOrderModuleList;// +0x08   
+    LIST_ENTRY  InInitializationOrderModuleList;// +0x10 
+    void* BaseAddress; // +0x18 
+    void* EntryPoint;  // +0x1c 
+    ULONG   SizeOfImage;
+    UNICODE_STRING FullDllName;
+    UNICODE_STRING BaseDllName;
+    ULONG   Flags;
+    SHORT   LoadCount;
+    SHORT   TlsIndex;
+    HANDLE  SectionHandle;
+    ULONG   CheckSum;
+    ULONG   TimeDateStamp;
+} LDR_MODULE, * PLDR_MODULE;
+
+
+typedef struct _PEB
+{
+    BOOLEAN InheritedAddressSpace;      // These four fields cannot change unless the
+    BOOLEAN ReadImageFileExecOptions;   //
+    BOOLEAN BeingDebugged;              //
+    BOOLEAN SpareBool;                  //
+    HANDLE Mutant;                      // INITIAL_PEB structure is also updated.
+
+    PVOID ImageBaseAddress;
+    PPEB_LDR_DATA Ldr;
+
+    // [...] this is a fragment, more elements follow here
+
+} PEB, * PPEB;
+
+#endif //__NTDLL_H__
+
+inline LPVOID get_module_by_name(WCHAR* module_name)
+{
+    PPEB peb = NULL;
+#if defined(_WIN64)
+    peb = (PPEB)__readgsqword(0x60);
+#else
+    peb = (PPEB)__readfsdword(0x30);
+#endif
+    PPEB_LDR_DATA ldr = peb->Ldr;
+    LIST_ENTRY list = ldr->InLoadOrderModuleList;
+
+    PLDR_MODULE Flink = *((PLDR_MODULE*)(&list));
+    PLDR_MODULE curr_module = Flink;
+
+    while (curr_module != NULL && curr_module->BaseAddress != NULL) {
+        if (curr_module->BaseDllName.Buffer == NULL) continue;
+        WCHAR* curr_name = curr_module->BaseDllName.Buffer;
+
+        size_t i = 0;
+        for (i = 0; module_name[i] != 0 && curr_name[i] != 0; i++) {
+            WCHAR c1, c2;
+            TO_LOWERCASE(c1, module_name[i]);
+            TO_LOWERCASE(c2, curr_name[i]);
+            if (c1 != c2) break;
+        }
+        if (module_name[i] == 0 && curr_name[i] == 0) {
+            //found
+            return curr_module->BaseAddress;
+        }
+        // not found, try next:
+        curr_module = (PLDR_MODULE)curr_module->InLoadOrderModuleList.Flink;
+    }
+    return NULL;
+}
+
+inline LPVOID get_func_by_name(LPVOID module, char* func_name)
+{
+    IMAGE_DOS_HEADER* idh = (IMAGE_DOS_HEADER*)module;
+    if (idh->e_magic != IMAGE_DOS_SIGNATURE) {
+        return NULL;
+    }
+    IMAGE_NT_HEADERS* nt_headers = (IMAGE_NT_HEADERS*)((BYTE*)module + idh->e_lfanew);
+    IMAGE_DATA_DIRECTORY* exportsDir = &(nt_headers->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT]);
+    if (exportsDir->VirtualAddress == NULL) {
+        return NULL;
+    }
+
+    DWORD expAddr = exportsDir->VirtualAddress;
+    IMAGE_EXPORT_DIRECTORY* exp = (IMAGE_EXPORT_DIRECTORY*)(expAddr + (ULONG_PTR)module);
+    SIZE_T namesCount = exp->NumberOfNames;
+
+    DWORD funcsListRVA = exp->AddressOfFunctions;
+    DWORD funcNamesListRVA = exp->AddressOfNames;
+    DWORD namesOrdsListRVA = exp->AddressOfNameOrdinals;
+
+    //go through names:
+    for (SIZE_T i = 0; i < namesCount; i++) {
+        DWORD* nameRVA = (DWORD*)(funcNamesListRVA + (BYTE*)module + i * sizeof(DWORD));
+        WORD* nameIndex = (WORD*)(namesOrdsListRVA + (BYTE*)module + i * sizeof(WORD));
+        DWORD* funcRVA = (DWORD*)(funcsListRVA + (BYTE*)module + (*nameIndex) * sizeof(DWORD));
+
+        LPSTR curr_name = (LPSTR)(*nameRVA + (BYTE*)module);
+        size_t k = 0;
+        for (k = 0; func_name[k] != 0 && curr_name[k] != 0; k++) {
+            if (func_name[k] != curr_name[k]) break;
+        }
+        if (func_name[k] == 0 && curr_name[k] == 0) {
+            //found
+            return (BYTE*)module + (*funcRVA);
+        }
+    }
+    return NULL;
+}
+
+int main()
+{
+    LPVOID base = get_module_by_name((const LPWSTR)L"kernel32.dll");
+    if (!base) {
+        return 1;
+    }
+
+    LPVOID load_lib = get_func_by_name((HMODULE)base, (LPSTR)"LoadLibraryA");
+    if (!load_lib) {
+        return 2;
+    }
+    LPVOID get_proc = get_func_by_name((HMODULE)base, (LPSTR)"GetProcAddress");
+    if (!get_proc) {
+        return 3;
+    }
+    HMODULE(WINAPI * _LoadLibraryA)(LPCSTR lpLibFileName) = (HMODULE(WINAPI*)(LPCSTR))load_lib;
+    FARPROC(WINAPI * _GetProcAddress)(HMODULE hModule, LPCSTR lpProcName) = (FARPROC(WINAPI*)(HMODULE, LPCSTR)) get_proc;
+
+    LPVOID u32_dll = _LoadLibraryA("user32.dll");
+
+    int (WINAPI * _MessageBoxW)(
+        _In_opt_ HWND hWnd,
+        _In_opt_ LPCWSTR lpText,
+        _In_opt_ LPCWSTR lpCaption,
+        _In_ UINT uType) = (int (WINAPI*)(
+            _In_opt_ HWND,
+            _In_opt_ LPCWSTR,
+            _In_opt_ LPCWSTR,
+            _In_ UINT)) _GetProcAddress((HMODULE)u32_dll, "MessageBoxW");
+
+    if (_MessageBoxW == NULL) return 4;
+
+    _MessageBoxW(0, L"Loaded!", L"OK", MB_OK);
+
+    return 0;
+}
